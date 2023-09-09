2340c7718b4cb4e36ed44ee78dd98aa3958f84f7
Marcos M. Junior <marcos.mth.jr@gmail.com>
2023-09-08 22:32:21 -0300

    Adicionando eslint

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/', ()=>{
-    return 'Hello World'
+app.get('/', () => {
+  return 'Hello World'
 })
 
-app.listen({
-    port: 3333
-}).then(()=>{
-    console.log("server running")
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('server running')
+  })
